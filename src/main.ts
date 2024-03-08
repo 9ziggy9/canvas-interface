0619696c717c3a9d8880b30a02ad3ccb467d1467
9ziggy9 <davidarogers@protonmail.com>
2024-03-08 12:00:07 -0800

    lolz

--- src/main.ts
+++ src/main.ts
@@ -80,12 +80,12 @@ function clearCanvas(ctx: CanvasRenderingContext2D, cnv: HTMLCanvasElement) {
 }
 
 type StateMap<T> = (state: T) => T;
+
 interface StateAggregator<T> {
   attach: (m: StateMap<T>) => StateAggregator<T>;
   run: () => void;
 }
 
-// MONAD !!!
 function returnStateAggregator<T>
   (initialState: T, fn: (s: T) => void) : StateAggregator<T>
 {
@@ -104,99 +104,129 @@ function returnStateAggregator<T>
   }
 }
 
-interface PolygonState {
-  sides: number;
-  dy_dt: number; // Vertical velocity
-  dx_dt: number; // horizontal velocity
-  radius: number;
-  center: [number, number]; // x, y coordinates of the center
-  lineWidth: number; 
-  strokeColor: string;
-  fillColor: string;
-  morphDir: number;
-  frameCount: number;
-}
-
 function animateAtTargetFPS(
   target: number,
   ctx: CanvasRenderingContext2D,
   cnv: HTMLCanvasElement,
 ) {
-
     let lastFrameTime = 0; 
-    let frameCount = 0;
 
-    const poly0: PolygonState = {
-      sides: 4,
-      dy_dt: 0,
-      dx_dt: 2,
-      radius: 40,
-      center: [cnv.width/2, cnv.height/2],
-      lineWidth: 4,
-      strokeColor: COLOR.red,
-      fillColor: COLOR.blue,
-      morphDir: 1,
-      frameCount: 0,
+    // things we wish to evolve in time
+    interface PolygonState {
+      position: [number, number];
+      radius: number;
+      dy_dt: number; // vertical velocity
+      dx_dt: number, // horizontal velocity
+      decay: number; // impact loss
+      frameCount: number;
+      sides: number;
+      morph: number;
     };
 
-    const updatePolygon = returnStateAggregator<PolygonState>(poly0, (s) => {
-      clearCanvas(ctx, cnv);
-      drawNGon(ctx, s.center, s.radius, s.sides, s.lineWidth, COLOR.red, COLOR.blue);
+    const g = 2;
+    const dt = 1;
+
+    const updatePosition = (s: PolygonState): PolygonState => ({
+      ...s, position: [s.position[0] + s.dx_dt, s.position[1] + s.dy_dt * dt]
     });
 
-    const applyGravity = (s: PolygonState): PolygonState => {
-      const dt = 1;
-      const g = 2;
-      s.dy_dt += g * dt;
-      s.center[1] += s.dy_dt * dt;
-      return s;
-    };
+    const gravity = (s: PolygonState): PolygonState => ({
+      ...s, dy_dt: s.dy_dt + g * dt
+    });
 
-    const enforceFloor = (s: PolygonState): PolygonState => {
-      let [_,y] = s.center;
-      if (y + s.radius > cnv.height) {
-        y = cnv.height - s.radius;
-        s.dy_dt *= -0.8; // energy loss
+    const applyFloor = (s: PolygonState): PolygonState =>
+      (s.position[1] + s.radius > cnv.height)
+        ? {
+            ...s,
+            position: [s.position[0], cnv.height - s.radius],
+            dy_dt: (-1) * s.decay * s.dy_dt,
+          }
+        : s;
+
+    const applyWalls = (s: PolygonState): PolygonState => ({
+      ...s,
+      dx_dt: s.dx_dt * ((s.position[0] + s.radius > cnv.width ||
+        s.position[0] - s.radius < 0) ? (-1) : 1)
+    });
+
+    const updateFrames = (s: PolygonState): PolygonState => ({
+      ...s, frameCount: s.frameCount + 1
+    });
+
+    const updateSides = (s: PolygonState): PolygonState => ({
+      ...s,
+      sides: s.sides + ((s.frameCount % 3 === 0) ? s.morph : 0),
+    });
+
+    const reverseMorph = (s: PolygonState): PolygonState => ({
+      ...s,
+      morph: s.morph * ((s.sides >= 10 || s.sides <= 3) ? -1 : 1),
+    });
+
+    const polygon00: PolygonState = {
+      position: [cnv.width/2, cnv.height/2],
+      radius: 40,
+      dy_dt: 0, // start from rest
+      dx_dt: 5,
+      decay: 1,
+      frameCount: 0,
+      sides: 3,
+      morph: 1,
+    };
+    const polygon0 = returnStateAggregator(polygon00, (s) => {
+      drawNGon(ctx, s.position, s.radius, s.sides, 4, COLOR.red, COLOR.blue);
+    });
+    polygon0
+      .attach(s => updateFrames(s))
+      .attach(s => gravity(s))
+      .attach(s => updatePosition(s))
+      .attach(s => applyFloor(s))
+      .attach(s => applyWalls(s))
+      .attach(s => updateSides(s))
+      .attach(s => reverseMorph(s));
+
+    const polygon10: PolygonState = {
+      position: [cnv.width/4, cnv.height/4],
+      radius: 20,
+      dy_dt: 0, // start from rest
+      dx_dt: -2,
+      decay: 1,
+      frameCount: 0,
+      sides: 3,
+      morph: 1,
+    };
+    const polygon1 = returnStateAggregator(polygon10, (s) => {
+      drawNGon(ctx, s.position, s.radius, s.sides, 4, COLOR.blue, COLOR.red);
+    });
+    polygon1
+      .attach(s => updateFrames(s))
+      .attach(s => gravity(s))
+      .attach(s => updatePosition(s))
+      .attach(s => applyFloor(s))
+      .attach(s => applyWalls(s))
+      .attach(s => updateSides(s))
+      .attach(s => reverseMorph(s));
+
+
+    function animationLoop(timestamp: number) {
+      const timeSinceLastFrame = timestamp - lastFrameTime;
+      const targetFrameTime = 1000 / target;
+      // Only update if enough time has passed for the next frame
+      if (timeSinceLastFrame >= targetFrameTime) {
+        lastFrameTime = timestamp;
+
+        // BEGIN: animations
+        clearCanvas(ctx, cnv);
+        polygon0.run();
+        polygon1.run();
+        // END: animations
+
+        requestAnimationFrame(animationLoop); 
+      } else {
+        requestAnimationFrame(animationLoop); // skip and wait
       }
-      return {...s, center: [s.center[0], y], dy_dt: s.dy_dt};
-    };
-
-    const morphSides = (s: PolygonState): PolygonState =>  {
-      if (s.frameCount % 5 === 0) s.sides++;
-      return {...s, sides: s.sides};
     }
-
-    const countFrames = (s: PolygonState): PolygonState => {
-      return {...s, frameCount: s.frameCount + 1};
-    }
-
-    const logState = (s: PolygonState): PolygonState => {
-      console.log(s);
-      return s;
-    }
- 
-    updatePolygon
-      .attach(s => countFrames(s))
-      .attach(s => applyGravity(s))
-      .attach(s => enforceFloor(s))
-      .attach(s => morphSides(s))
-
-  function animationLoop(timestamp: number) {
-    const timeSinceLastFrame = timestamp - lastFrameTime;
-    const targetFrameTime = 1000 / target;
-    // Only update if enough time has passed for the next frame
-    if (timeSinceLastFrame >= targetFrameTime) {
-      lastFrameTime = timestamp;
-      frameCount++;
-
-      updatePolygon.run();
-
-      requestAnimationFrame(animationLoop); 
-    } else {
-      requestAnimationFrame(animationLoop); // skip and wait
-    }
-  }
-  requestAnimationFrame(animationLoop);
+    requestAnimationFrame(animationLoop);
 }
 
 function main(): void {
