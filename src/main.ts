e07d299f312b57f0bdeaee3292c6c40a61975557
9ziggy9 <davidarogers@protonmail.com>
2024-03-12 07:47:54 -0700

    gif examples

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,11 @@ function clearCanvas
   );
 }
 
-function main(): void {
+window.onload = main_dumbells_ex;
+
+
+// Further examples
+function main_spring_ex(): void {
   const [ctx, cnv] = initCanvas("main-canvas");
 
   const c0: Circle.State = {
@@ -77,176 +81,120 @@ function main(): void {
   );
 }
 
-window.onload = main;
+function main_dumbells_ex(): void {
+  const [ctx, cnv] = initCanvas("main-canvas");
 
+  const dumbells = makeDumbells(ctx, cnv);
 
-// Further examples
-// function main_dumbells(): void {
-//   const [ctx, cnv] = initCanvas("main-canvas");
+  Loop.animateAtTargetFPS(
+    60, ctx, cnv,
+    () => {
+      clearCanvas(ctx, cnv);
+      dumbells.forEach(d => d.run());
+    }
+  );
+}
 
-//   const [circle_pair1, circle_pair2, circle_pair3] = makeDumbells(ctx, cnv);
+function main_polygon_ex(): void {
+  const [ctx, cnv] = initCanvas("main-canvas");
 
-//   Loop.animateAtTargetFPS(
-//     60, ctx, cnv,
-//     () => {
-//       clearCanvas(ctx, cnv);
-//       circle_pair1.run();
-//       circle_pair2.run();
-//       circle_pair3.run();
-//     }
-//   );
-// }
+  const polygon0 = State.unit<Polygon.State>({
+    ...Polygon.defaults,
+    position:    [cnv.width/2, cnv.height/2],
+    fillColor:   Color.red,
+    borderColor: Color.blue,
+    dx_dt: 3,
+  }, (s) => {
+    Primitives.drawNGon(
+      ctx, s.position, s.radius, s.sides, 4, s.fillColor, s.borderColor
+    );
+  })
+  .attach(s => Kinematic.updateFrames(s))
+  .attach(s => Forces.verticalGravity(s, 2))
+  .attach(s => Kinematic.updatePosition(s))
+  .attach(s => Bounded.applyFloor(s, cnv))
+  .attach(s => Bounded.applyWalls(s, cnv))
+  .attach(s => Polygon.reverseMorph(s))
+  .attach(s => Polygon.updateSides(s));
 
-// function main_polygon_ex(): void {
-//   const [ctx, cnv] = initCanvas("main-canvas");
+  Loop.animateAtTargetFPS(
+    60, ctx, cnv,
+    () => {
+      clearCanvas(ctx, cnv);
+      polygon0.run();
+    }
+  );
+}
 
-//   const polygon0 = State.unit<Polygon.State>({
-//     ...Polygon.defaults,
-//     position:    [cnv.width/2, cnv.height/2],
-//     fillColor:   Color.red,
-//     borderColor: Color.blue,
-//     dx_dt: 3,
-//   }, (s) => {
-//     Primitives.drawNGon(
-//       ctx, s.position, s.radius, s.sides, 4, s.fillColor, s.borderColor
-//     );
-//   })
-//   .attach(s => Kinematic.updateFrames(s))
-//   .attach(s => Forces.verticalGravity(s, 2))
-//   .attach(s => Kinematic.updatePosition(s))
-//   .attach(s => Bounded.applyFloor(s, cnv))
-//   .attach(s => Bounded.applyWalls(s, cnv))
-//   .attach(s => Polygon.reverseMorph(s))
-//   .attach(s => Polygon.updateSides(s));
+const randomVelocity = (): number => (1 - 2 * Number((Math.random() > 0.5)))
+  * Math.floor(Math.random() * 20)
 
-//   Loop.animateAtTargetFPS(
-//     60, ctx, cnv,
-//     () => {
-//       clearCanvas(ctx, cnv);
-//       polygon0.run();
-//     }
-//   );
-// }
-
-// function makeDumbells(ctx: CanvasRenderingContext2D, cnv: HTMLCanvasElement) {
-//   const circle_pair1 = State.unit<Circle.State[]>([
-//     {
-//       ...Circle.defaults,
-//       position:    [cnv.width/2, cnv.height/2],
-//       fillColor:   Color.blue,
-//       borderColor: Color.red,
-//       dx_dt: 7,
-//       dy_dt: 2,
-//       yDissipation: 0.8, xDissipation: 0.3,
-//     },
-//     {
-//       ...Circle.defaults,
-//       position:    [cnv.width/2, cnv.height/4],
-//       fillColor:   Color.red,
-//       borderColor: Color.blue,
-//       yDissipation: 0.8, xDissipation: 0.3,
-//     }
-//   ], ([c0,c1]) => {
-//     ctx.beginPath();
-//     ctx.moveTo(c0.position[0], c0.position[1]);
-//     ctx.lineTo(c1.position[0], c1.position[1]);
-//     ctx.strokeStyle = Color.darkGray;
-//     ctx.stroke();
-//     Primitives.drawCircle(
-//       ctx, [c0.position[0], c0.position[1]], c0.radius,
-//       c0.fillColor, c0.borderColor, c0.borderWidth,
-//     );
-//     Primitives.drawCircle(
-//       ctx, [c1.position[0], c1.position[1]], c1.radius,
-//       c1.fillColor, c1.borderColor, c1.borderWidth,
-//     );
-//   })
-//     .attach(circles    => circles.map(c => Kinematic.updateFrames(c)))
-//     .attach(circles    => circles.map(c => Forces.verticalGravity(c, 5)))
-//     .attach(circles    => circles.map(c => Kinematic.updatePosition(c)))
-//     .attach(([c0, c1]) => Forces.fixedDistanceConstraint(c0, c1, 100))
-//     .attach(circles    => circles.map(c => Bounded.applyFloor(c, cnv)))
-//     .attach(circles    => circles.map(c => Bounded.applyWalls(c, cnv)));
-
-//   const circle_pair2 = State.unit<Circle.State[]>([
-//     {
-//       ...Circle.defaults,
-//       position:    [cnv.width/3, cnv.height/3],
-//       fillColor:   Color.blue,
-//       borderColor: Color.red,
-//       dx_dt: -1,
-//       dy_dt: 3,
-//       yDissipation: 0.8, xDissipation: 0.3,
-//     },
-//     {
-//       ...Circle.defaults,
-//       position:    [cnv.width/3, cnv.height/3 + 20],
-//       fillColor:   Color.red,
-//       borderColor: Color.blue,
-//       yDissipation: 0.8, xDissipation: 0.3,
-//     }
-//   ], ([c0,c1]) => {
-//     ctx.beginPath();
-//     ctx.moveTo(c0.position[0], c0.position[1]);
-//     ctx.lineTo(c1.position[0], c1.position[1]);
-//     ctx.strokeStyle = Color.darkGray;
-//     ctx.stroke();
-//     Primitives.drawCircle(
-//       ctx, [c0.position[0], c0.position[1]], c0.radius,
-//       c0.fillColor, c0.borderColor, c0.borderWidth,
-//     );
-//     Primitives.drawCircle(
-//       ctx, [c1.position[0], c1.position[1]], c1.radius,
-//       c1.fillColor, c1.borderColor, c1.borderWidth,
-//     );
-//   })
-//     .attach(circles    => circles.map(c => Kinematic.updateFrames(c)))
-//     .attach(circles    => circles.map(c => Forces.verticalGravity(c, 1)))
-//     .attach(circles    => circles.map(c => Kinematic.updatePosition(c)))
-//     .attach(([c0, c1]) => Forces.fixedDistanceConstraint(c0, c1, 100))
-//     .attach(circles    => circles.map(c => Bounded.applyFloor(c, cnv)))
-//     .attach(circles    => circles.map(c => Bounded.applyWalls(c, cnv)));
-
-//   const circle_pair3 = State.unit<Circle.State[]>([
-//     {
-//       ...Circle.defaults,
-//       position:    [200, 240],
-//       fillColor:   Color.blue,
-//       borderColor: Color.red,
-//       radius: 10,
-//       dx_dt: -30,
-//       dy_dt: 20,
-//       yDissipation: 0.8, xDissipation: 0.3,
-//     },
-//     {
-//       ...Circle.defaults,
-//       position:    [210, 250],
-//       radius: 10,
-//       fillColor:   Color.red,
-//       borderColor: Color.blue,
-//       yDissipation: 0.8, xDissipation: 0.3,
-//     }
-//   ], ([c0,c1]) => {
-//     ctx.beginPath();
-//     ctx.moveTo(c0.position[0], c0.position[1]);
-//     ctx.lineTo(c1.position[0], c1.position[1]);
-//     ctx.strokeStyle = Color.darkGray;
-//     ctx.stroke();
-//     Primitives.drawCircle(
-//       ctx, [c0.position[0], c0.position[1]], c0.radius,
-//       c0.fillColor, c0.borderColor, c0.borderWidth,
-//     );
-//     Primitives.drawCircle(
-//       ctx, [c1.position[0], c1.position[1]], c1.radius,
-//       c1.fillColor, c1.borderColor, c1.borderWidth,
-//     );
-//   })
-//     .attach(circles    => circles.map(c => Kinematic.updateFrames(c)))
-//     .attach(circles    => circles.map(c => Forces.verticalGravity(c, 3)))
-//     .attach(circles    => circles.map(c => Kinematic.updatePosition(c)))
-//     .attach(([c0, c1]) => Forces.fixedDistanceConstraint(c0, c1, 100))
-//     .attach(circles    => circles.map(c => Bounded.applyFloor(c, cnv)))
-//     .attach(circles    => circles.map(c => Bounded.applyWalls(c, cnv)));
-
-//   return [circle_pair1, circle_pair2, circle_pair3];
-// }
+function makeDumbells(ctx: CanvasRenderingContext2D, cnv: HTMLCanvasElement) {
+  const xs: number[] = (new Array(100))
+                         .fill(10)
+                         .reduce((acc, _, n) => [...acc, n + 10], [])
+  return xs
+    .map(p => State.unit({
+      c0: {
+        ...Circle.defaults,
+        position:    [p, p+10],
+        fillColor:   Color.blue,
+        borderColor: Color.red,
+        radius: 10,
+        dx_dt: randomVelocity(),
+        dy_dt: randomVelocity(),
+        yDissipation: Math.random(), xDissipation: Math.random(),
+      },
+      c1: {
+        ...Circle.defaults,
+        position:    [p+10, p+20],
+        radius: 10,
+        fillColor:   Color.red,
+        borderColor: Color.blue,
+        dx_dt: randomVelocity(),
+        dy_dt: randomVelocity(),
+        yDissipation: Math.random(), xDissipation: Math.random(),
+      }
+    }, ({c0,c1}) => {
+      ctx.beginPath();
+      ctx.moveTo(c0.position[0], c0.position[1]);
+      ctx.lineTo(c1.position[0], c1.position[1]);
+      ctx.strokeStyle = Color.darkGray;
+      ctx.stroke();
+      Primitives.drawCircle(
+        ctx, [c0.position[0], c0.position[1]], c0.radius,
+        c0.fillColor, c0.borderColor, c0.borderWidth,
+      );
+      Primitives.drawCircle(
+        ctx, [c1.position[0], c1.position[1]], c1.radius,
+        c1.fillColor, c1.borderColor, c1.borderWidth,
+      );
+    })
+      .attach(({c0, c1}) => ({
+        c0: Kinematic.updateFrames(c0),
+        c1: Kinematic.updateFrames(c1),
+      }))
+      .attach(({c0, c1}) => ({
+        c0: Forces.verticalGravity(c0, 3),
+        c1: Forces.verticalGravity(c1, 3),
+      }))
+      .attach(({c0, c1}) => ({
+        c0: Kinematic.updatePosition(c0),
+        c1: Kinematic.updatePosition(c1),
+      }))
+      .attach(({c0, c1}) => {
+        const [nc0, nc1] = Forces.fixedDistanceConstraint(c0, c1, 100);
+        return {
+          c0: nc0,
+          c1: nc1,
+        }
+      })
+      .attach(({c0, c1}) => ({
+        c0: Bounded.applyFloor(c0, cnv),
+        c1: Bounded.applyFloor(c1, cnv),
+      }))
+      .attach(({c0, c1}) => ({
+        c0: Bounded.applyWalls(c0, cnv),
+        c1: Bounded.applyWalls(c1, cnv),
+      })));
+}
